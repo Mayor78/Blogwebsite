e57d4c4c124ab9021d6b8b22c5ba5a06b1df97f5
Abraham mayowa <Abrahammayowa78@gmail.com>
2024-06-11 13:22:33 +0100

    ba3 low

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { FaBars } from "react-icons/fa";
+import { AiOutlineClose } from 'react-icons/ai'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <nav>
+        <div className="container nav_container">
+            <Link to="/" className=' w-[3rem] block'>
+                <img src="https://i.ibb.co/0222222/logo.png" alt="logo" className="nav_logo_img"  />
+            </Link>
+            <ul>
+                <li><Link to="/profile">mayor</Link></li>
+                <li><Link to="/create">CreaTe Post</Link></li>
+                <li><Link to="/authors">Authors</Link></li>
+                <li><Link to="/logout">Logout</Link></li>
+            </ul>
+             <button>
+                <AiOutlineClose/>
+             </button>
+        </div>
+    </nav>
   )
 }
 
